90012566328157ed6dfcec663ffa124db428a94d
Radoslaw Choluj <radekcholuj@wp.pl>
2021-03-21 00:57:33 +0100

    #7 throws przekazanie odpowiedzialności w klasie math

---
+++ src/pl/radekcholuj/exceptions/throwsexceptions/Math.java
@@ -0,0 +1,8 @@
+package pl.radekcholuj.exceptions.throwsexceptions;
+
+public final class Math {
+
+    public static int div(int a, int b) throws ArithmeticException{
+        return a/b;
+    }
+}
