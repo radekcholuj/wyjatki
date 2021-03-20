1712042ff4c9935421420f77f2f98fa07d3b6337
Radoslaw Choluj <radekcholuj@wp.pl>
2021-03-20 22:55:12 +0100

    #1 program rzucający wyjątek ArithmeticException

---
+++ src/pl/radekcholuj/exceptions/FirstExceptions.java
@@ -0,0 +1,11 @@
+package pl.radekcholuj.exceptions;
+
+public class FirstExceptions {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 0;
+        int result = a/b;
+        result+=10;
+        System.out.println(result);
+    }
+}
