bb6d4f4addb91abe2379ce741e75e2c848afe5d3
Radoslaw Choluj <radekcholuj@wp.pl>
2021-03-21 00:14:48 +0100

    #5 obsłużenie wyjątku oznaczonego i nieoznaczonego

--- src/pl/radekcholuj/exceptions/FirstExceptions.java
+++ src/pl/radekcholuj/exceptions/FirstExceptions.java
@@ -1,16 +1,25 @@
 package pl.radekcholuj.exceptions;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class FirstExceptions {
     public static void main(String[] args) {
         int a = 5;
         int b = 0;
         // niezonaczony
-        int result = a/b;
-        result+=10;
-        System.out.println(result);
+        try {
+            int result = a / b;
+            result += 10;
+            System.out.println(result);
+        } catch (ArithmeticException e){
+            System.out.println("Not divide by 0.");
+        }
         // oznaczony
-        FileInputStream fileInputStream = new FileInputStream("/to/ma/byc/sciezka/plik.text");
+        try {
+            FileInputStream fileInputStream = new FileInputStream("/to/ma/byc/sciezka/plik.text");
+        } catch (FileNotFoundException e){
+            System.out.println("Not exists.");
+        }
     }
 }
